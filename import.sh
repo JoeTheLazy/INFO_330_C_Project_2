4c1ce5fe50045b6f5660be31ef4e7151d0d7fa9a
eddy <hjb19971101@gmail.com>
2018-11-29 15:52:22 -0800

    import command

---
+++ import.sh
@@ -0,0 +1,4 @@
+mongoimport --db project2 --collection Customers --file Customers.json
+mongoimport --db project2 --collection LineItems --file LineItems.json
+mongoimport --db project2 --collection Orders --file Orders.json
+mongoimport --db project2 --collection Products --file Products.json
